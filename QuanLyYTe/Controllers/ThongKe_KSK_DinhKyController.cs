1d4b3561fcc404aabdd5592447186a7764037fc7
baodiep105 <diephuynhbaobao1@gmal.com>
2025-02-27 16:38:33 +0700

    fix loi export thong ke kham dinh ky

--- QuanLyYTe/Controllers/ThongKe_KSK_DinhKyController.cs
+++ QuanLyYTe/Controllers/ThongKe_KSK_DinhKyController.cs
@@ -12,11 +12,14 @@ namespace QuanLyYTe.Controllers
     public class ThongKe_KSK_DinhKyController : Controller
     {
         private readonly DataContext _context;
-
-        public ThongKe_KSK_DinhKyController(DataContext _context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ThongKe_KSK_DinhKyController(DataContext _context, IWebHostEnvironment webHostEnvironment)
         {
             this._context = _context;
+            _webHostEnvironment = webHostEnvironment;
         }
+        
+        
         public async Task<IActionResult> Index(DateTime? begind, DateTime? endd, int page = 1)
         {
             DateTime Now = DateTime.Now;
@@ -66,20 +69,24 @@ namespace QuanLyYTe.Controllers
                 res = res.Where(x => x.NgayKSK >= begind && x.NgayKSK <= endd).ToList();
             }
             const int pageSize = 10000;
+            var bp_nm = _context.PhongBan.ToList();
             if (page < 1)
             {
                 page = 1;
             }
-            int resCount = res.Count;
+            int resCount = bp_nm.Count;
+            ViewData["tong"] = resCount;
             var pager = new Pager(resCount, page, pageSize);
             int recSkip = (page - 1) * pageSize;
-            var data = res.Skip(recSkip).Take(pager.PageSize).ToList();
+            var data = bp_nm.Skip(recSkip).Take(pager.PageSize).ToList();
             this.ViewBag.Pager = pager;
-            var bp_nm = _context.PhongBan.ToList();
-            ViewData["PhongBan"] = bp_nm;
+            
+            ViewData["data"] = res;
             var ct_pl = _context.PhanLoaiKSK.ToList();
             ViewData["PhanLoaiKSK"] = ct_pl;
-            return View(data);
+            ViewData["endd"] = endd?.ToString("yyyy-MM-dd");
+            ViewData["begind"] = begind?.ToString("yyyy-MM-dd");
+            return View(bp_nm);
 
         }
 
@@ -89,86 +96,75 @@ namespace QuanLyYTe.Controllers
             try
             {
 
-                string fileNamemau = AppDomain.CurrentDomain.DynamicDirectory + @"App_Data\Thong ke.xlsx";
-                string fileNamemaunew = AppDomain.CurrentDomain.DynamicDirectory + @"App_Data\Thong ke_Temp.xlsx";
-                XLWorkbook Workbook = new XLWorkbook(fileNamemau);
-                IXLWorksheet Worksheet = Workbook.Worksheet("TD");
-                var Data = _context.PhongBan.ToList();
-                int row = 5, stt = 0, icol = 1, icol_ = 1, row_ = 5;
+                string path = "Form files/BM_KSK_DinhKy.xlsx";
+                HttpContext.Response.ContentType = "application/xlsx";
+                string filePath = Path.Combine(_webHostEnvironment.ContentRootPath, path);
 
-                DateTime Begin = (DateTime)begind;
-                string TuNgay = Begin.ToString("MM");
-                DateTime End = (DateTime)endd;
-                string DenNgay = End.ToString("MM");
-                int SoThang = Convert.ToInt32(DenNgay) - Convert.ToInt32(TuNgay);
-                if (Data.Count > 0)
+                if (!System.IO.File.Exists(filePath))
                 {
+                    return null; // Xử lý lỗi nếu file không tồn tại
+                }
+                
+                DateTime Now = DateTime.Now;
+                DateTime startDay = new DateTime(Now.Year, Now.Month, 1);
+                DateTime endDay = startDay.AddMonths(1).AddDays(-1);
+                var res = await (from a in _context.KSK_DinhKy
+                                 join nv in _context.NhanVien on a.ID_NV equals nv.ID_NV
+                                 join pb in _context.PhongBan on nv.ID_PhongBan equals pb.ID_PhongBan
+                                 join gt in _context.GioiTinh on a.ID_GioiTinh equals gt.ID_GioiTinh
+                                 join l in _context.PhanLoaiKSK on a.ID_PhanLoaiKSK equals l.ID_PhanLoaiKSK
+                                 join m in _context.NhomMau on a.ID_NhomMau equals m.ID_NhomMau into ulist1
+                                 from m in ulist1.DefaultIfEmpty()
+                                 select new KSK_DinhKy
+                                 {
+                                     MaNV = nv.MaNV,
+                                     HoVaTen = nv.HoTen,
+                                     TenGioiTinh = gt.TenGioiTinh,
+                                     KhamTongQuat = a.KhamTongQuat,
+                                     KhamPhuKhoa = a.KhamPhuKhoa,
+                                     TenNhomMau = m.TenNhomMau,
+                                     NhomMauRh = a.NhomMauRh,
+                                     CongThucMau = a.CongThucMau,
+                                     NuocTieu = a.NuocTieu,
+                                     TenLoaiKSK = l.TenLoaiKSK,
+                                     KetLuanKSK = a.KetLuanKSK,
+                                     NgayKSK = (DateTime)a.NgayKSK
 
-
-
-                    foreach (var item in Data)
-                    {
-                        row++; stt++; icol = 1; icol_ = 1;
-
-                        Worksheet.Cell(row, icol).Value = stt;
-                        Worksheet.Cell(row, icol).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                        Worksheet.Cell(row, icol).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-
-                        icol++;
-                        Worksheet.Cell(row, icol).Value = item.TenPhongBan;
-                        Worksheet.Cell(row, icol).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Left;
-                        Worksheet.Cell(row, icol).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-
-                        for (int i = 0; i <= SoThang; i++)
-                        {
-                            DateTime Now = Begin.AddMonths(i);
-                            DateTime startDay = new DateTime(Now.Year, Now.Month, 1);
-                            DateTime endDay = startDay.AddMonths(1).AddDays(-1);
-
-                            icol_++;
-                            Worksheet.Cell(row_, (icol_ + 1)).Value = startDay.ToString("MM/yyyy");
-                            Worksheet.Cell(row_, (icol_ + 1)).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                            Worksheet.Cell(row_, (icol_ + 1)).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-                            Worksheet.Cell(row_, (icol_ + 1)).Style.Alignment.WrapText = true;
-
-
-                            var Count = (from a in _context.KSK_DinhKy
-                                         join nv in _context.NhanVien on a.ID_NV equals nv.ID_NV
-                                         join pb in _context.PhongBan on nv.ID_PhongBan equals pb.ID_PhongBan
-                                         select new KSK_DinhKy
-                                         {
-                                             ID_KSK_DK = a.ID_KSK_DK,
-                                             ID_NV = a.ID_NV,
-                                             ID_PhongBan = (int)nv.ID_PhongBan,
-                                             NgayKSK = (DateTime)a.NgayKSK
-
-                                         }).Where(x => x.ID_PhongBan == item.ID_PhongBan && x.NgayKSK >= startDay && x.NgayKSK <= endDay).Count();
-
-                            Worksheet.Cell(row, (icol_ + 1)).Value = Count;
-                            Worksheet.Cell(row, (icol_ + 1)).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Left;
-                            Worksheet.Cell(row, (icol_ + 1)).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-                        }
-                    }
-
-                    Worksheet.Range("A6:N" + (row)).Style.Font.SetFontName("Times New Roman");
-                    Worksheet.Range("A6:N" + (row)).Style.Font.SetFontSize(13);
-                    Worksheet.Range("A6:N" + (row)).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
-                    Worksheet.Range("A6:N" + (row)).Style.Border.InsideBorder = XLBorderStyleValues.Thin;
-
-
-                    Workbook.SaveAs(fileNamemaunew);
-                    byte[] fileBytes = System.IO.File.ReadAllBytes(fileNamemaunew);
-                    string fileName = "Thống kê KSK BNN - " + DateTime.Now.Date.ToString("dd/MM/yyyy") + ".xlsx";
-                    return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+                                 }).ToListAsync();
+                if (begind == null && endd == null)
+                {
+                    res = res.Where(x => x.NgayKSK >= startDay && x.NgayKSK <= endDay).ToList();
                 }
                 else
                 {
-
-
-                    Workbook.SaveAs(fileNamemaunew);
-                    byte[] fileBytes = System.IO.File.ReadAllBytes(fileNamemaunew);
-                    string fileName = "Thống kê KSK BNN - " + DateTime.Now.Date.ToString("dd/MM/yyyy") + ".xlsx";
-                    return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+                    res = res.Where(x => x.NgayKSK >= begind && x.NgayKSK <= endd).ToList();
+                }
+                using (var workbook = new XLWorkbook(filePath))
+                {
+                    var worksheet = workbook.Worksheet(1);
+                    for (int i = 0; i < res.Count(); i++)
+                    {
+                        worksheet.Cell(i + 6, 1).Value = i + 1;
+                        worksheet.Cell(i + 6, 2).Value = res[i].MaNV;
+                        worksheet.Cell(i + 6, 3).Value = res[i].HoVaTen;
+                        worksheet.Cell(i + 6, 4).Value = res[i].TenGioiTinh;
+                        worksheet.Cell(i + 6, 5).Value = res[i].KhamTongQuat;
+                        worksheet.Cell(i + 6, 6).Value = res[i].KhamPhuKhoa;
+                        worksheet.Cell(i + 6, 7).Value = res[i].TenNhomMau;
+                        worksheet.Cell(i + 6, 8).Value = res[i].NhomMauRh;
+                        worksheet.Cell(i + 6, 9).Value = res[i].CongThucMau;
+                        worksheet.Cell(i + 6, 10).Value = res[i].NuocTieu;
+                        worksheet.Cell(i + 6, 11).Value = res[i].TenLoaiKSK;
+                        worksheet.Cell(i + 6, 12).Value = res[i].KetLuanKSK;
+                        worksheet.Cell(i + 6, 13).Value = res[i].NgayKSK;
+                    }
+                    // Lưu lại file Excel
+                    using (var stream = new MemoryStream())
+                    {
+                        workbook.SaveAs(stream);
+                        stream.Seek(0, SeekOrigin.Begin);
+                        return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", path);
+                    }
                 }
             }
             catch (Exception ex)
